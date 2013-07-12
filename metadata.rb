38681f2047b87eed0ede19fce5f10029e0f20d71
James Cuzella <james.cuzella@lyraphase.com>
2013-07-12 17:06:11 -0600

    Squashed 'cookbooks/strainer-berkshelf-chefspec-recursive-bug/' content from commit 3b3491f
    git-subtree-dir: cookbooks/strainer-berkshelf-chefspec-recursive-bug
    git-subtree-split: 3b3491ff417d41601f3cbd6f541765f9efd5c323

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'strainer-berkshelf-chefspec-recursive-bug'
+maintainer       'TangoGroup'
+maintainer_email 'cookbooks@tangogroup.com'
+license          'Apache 2.0'
+description      'Installs/Configures strainer-berkshelf-chefspec-recursive-bug'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'chef-solo-search' # Test dependency, or if using chef-solo
